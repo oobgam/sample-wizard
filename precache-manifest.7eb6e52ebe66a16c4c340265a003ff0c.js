faaf5bc60394b7590bfdbdba65ec552dd5e7d9a2
RD Magboo <rdmagboo@gmail.com>
2018-12-04 15:53:32 +0800

    Updates

---
+++ precache-manifest.7eb6e52ebe66a16c4c340265a003ff0c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c01e2c1925dcfc99041",
+    "url": "/sample-wizard/static/js/runtime~main.7c01e2c1.js"
+  },
+  {
+    "revision": "588e8108148c9b1e605c",
+    "url": "/sample-wizard/static/js/main.588e8108.chunk.js"
+  },
+  {
+    "revision": "98286b79f588941b4f30",
+    "url": "/sample-wizard/static/js/1.98286b79.chunk.js"
+  },
+  {
+    "revision": "8a05c96d8029fae9709e78a5f635e09c",
+    "url": "/sample-wizard/index.html"
+  }
+];
